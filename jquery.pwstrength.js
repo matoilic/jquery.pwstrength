ae73381f9790e1f9e7080834795906b097c0ffc9
Mato Ilic <info@matoilic.ch>
2012-01-10 15:02:23 +0100

    strength measurement

--- jquery.pwstrength.js
+++ jquery.pwstrength.js
@@ -10,5 +10,40 @@
  * http://www.gnu.org/licenses/gpl.html
  */
 ;(function($) {
+    $.pwstrength = function(password) {
+        var score = 0, length = password.length, upperCase, lowerCase, digits, nonAlpha;
+        
+        if(length < 5) score += 3;
+        else if(length < 8) score += 6;
+        else if(length < 16) score += 12;
+        else score += 18;
+        
+        lowerCase = password.match(/[a-z]/g);
+        if(lowerCase) score += 1;
+        
+        upperCase = password.match(/[A-Z]/g);
+        if(upperCase) score += 5;
+        
+        if(upperCase && lowerCase) score += 2;
+        
+        digits = password.match(/\d/g);
+        if(digits && digits.length) score += 5;
+        
+        nonAlpha = password.match(/\W/g)
+        if(nonAlpha) score += (nonAlpha.length > 1) ? 10 : 5;
+        
+        if(upperCase && lowerCase && digits && nonAlpha) score += 5;
+
+        if(password.match(/\s/)) score += 10;
+
+        if(score < 16) return 0;
+        if(score < 25) return 1;
+        if(score < 35) return 2;
+        if(score < 45) return 3;
+        return 4;
+    };
     
+    $.fn.pwstrength = function() {
+        
+    };
 })(jQuery);
--- spec/pwstrength.js
+++ spec/pwstrength.js
@@ -1,3 +1,22 @@
-describe("jquery.pwstrength", function() {
+describe("jquery.pwstrength", function() {    
+    it("detects very weak passwords", function() {
+        expect($.pwstrength('abcd')).toEqual(0);
+    });
     
+    it("detects weak passwords", function() {
+        expect($.pwstrength('abdge123')).toEqual(1);
+    });
+    
+    it("detects mediocre passwords", function() {
+        expect($.pwstrength('Ab12Degf')).toEqual(2);
+    });
+    
+    it("detects strong passwords", function() {
+        expect($.pwstrength('@b12Degf')).toEqual(3);
+    });
+    
+    it("detects very strong passwords", function() {
+        expect($.pwstrength('@b12De Fghbe#')).toEqual(4);
+        expect($.pwstrength('@b12DeFghbe234567#')).toEqual(4);
+    });
 });
